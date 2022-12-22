178c93cb6c98c035e57f43e3cd8bb1639410cce1
recillah khamala <recillahk@gmail.com>
2022-12-23 02:29:52 +0300

    Create has-many, belongs to relationship between book and rental

--- src/book.rb
+++ src/book.rb
@@ -1,11 +1,19 @@
+require_relative './rental'
+
 # Create a class Book 
 class Book
   # Setters and getters for instance variables
-  attr_accessor :title, :author
+  attr_accessor :title, :author 
+  attr_reader :rentals
 
   #   @title and @author instance variables, should be initialized in the constructor.
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def add_rental(person, date)
+    Rental.new(date, self, person)
   end
 end
--- src/rental.rb
+++ src/rental.rb
@@ -1,9 +1,15 @@
 # Create a class Rental
 class Rental
   #Setter and getter for @date
-  attr_accessor :date
+  attr_accessor :date, :book, :person
 
-  def initialize(date)
+  def initialize(date, book, person)
     @date = date
+
+    @book = book
+    @book.rentals << self
+
+    @person = person
+    @person.rentals << self
   end
 end
