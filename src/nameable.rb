cc36a651407244cabeefbc2f98e7410b3b23bef3
recillah khamala <recillahk@gmail.com>
2022-12-22 20:11:58 +0300

    Create class nameable in the nameable file

---
+++ src/nameable.rb
@@ -0,0 +1,7 @@
+# Create a class Nameable.
+class Nameable
+  # Implement a method called correct_name that will raise a NotImplementedError.
+  def correct_name
+    raise NotImplementedError
+  end
+end
