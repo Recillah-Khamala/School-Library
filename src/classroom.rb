b7c06fde54a2c4d2cfc6fc6cff19ab177caf022e
recillah khamala <recillahk@gmail.com>
2022-12-23 01:19:21 +0300

    Create the has-many/belongs-to relationship between Classroom and Student

--- src/classroom.rb
+++ src/classroom.rb
@@ -2,9 +2,17 @@
 class Classroom
   # Setter and getter for @label
   attr_accessor :label
+  attr_reader :students
 
   # @label instance variable, should be initialized in the constructor.
   def initialize(label)
     @label = label
+    @students = []
+  end
+
+  #   Create the has-many side (a classroom has many students).
+  def add_student(Student)
+    @students.push(student) unless @students.include?(student)
+    student.classroom = self
   end
 end
--- src/student.rb
+++ src/student.rb
@@ -1,7 +1,7 @@
 require_relative './person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(age, classroom, name = 'unknown', parent_permission = 'true')
     @classroom = classroom
@@ -10,5 +10,11 @@ class Student < Person
 
   def play_hooky
     '¯(ツ)/¯'
+  end
+
+  #   Create the belongs-to side (a student belongs to a classroom).
+  def classroom = (classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
