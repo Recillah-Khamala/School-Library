0f030ec0f5e14b3a433a529343918cfff1c95f23
recillah khamala <recillahk@gmail.com>
2023-01-02 21:25:22 +0300

    Fix linters error

--- src/person.rb
+++ src/person.rb
@@ -7,7 +7,7 @@ class Person < Nameable
   attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(age, name = 'unknown', parent_permission = true)
+  def initialize(age, name = 'unknown', parent_permission = 'true')
     super()
 
     @id = Random.rand(1...1000)
